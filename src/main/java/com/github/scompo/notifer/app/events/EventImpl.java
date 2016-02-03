098d54f3783139194ea6dd2a54552962cec1c1de
Mauro Scomparin <scompo@gmail.com>
2016-02-03 22:25:31 +0100

    added EventImpl

---
+++ src/main/java/com/github/scompo/notifer/app/events/EventImpl.java
@@ -0,0 +1,42 @@
+package com.github.scompo.notifer.app.events;
+
+import java.time.ZonedDateTime;
+
+import com.github.scompo.notifer.api.events.Event;
+
+/**
+ * {@link Event} implementation.
+ */
+public class EventImpl implements Event{
+	
+	private ZonedDateTime start;
+	
+	private String name;
+	
+	public EventImpl(String name, ZonedDateTime start) {
+		
+		this.name = name;
+		this.start = start;
+	}
+
+	@Override
+	public String getName() {
+		
+		return name;
+	}
+
+	@Override
+	public ZonedDateTime getStart() {
+		return start;
+	}
+
+	@Override
+	public void setStart(ZonedDateTime start) {
+		this.start = start;
+	}
+
+	@Override
+	public String toString() {
+		return "EventImpl [start=" + start + ", name=" + name + "]";
+	}
+}
