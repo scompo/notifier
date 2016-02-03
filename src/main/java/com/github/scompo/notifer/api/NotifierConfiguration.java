538b7cf01c6e7ae6e18e8f1e45c1ba9a560b7a41
Mauro Scomparin <scompo@gmail.com>
2016-02-03 21:37:34 +0100

    used notifier-api

--- src/main/java/com/github/scompo/notifer/api/NotifierConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.github.scompo.notifer.api;
-
-import com.github.scompo.utils.startable.Startable;
-
-public interface NotifierConfiguration extends Startable{
-
-	void doConfiguration();
-
-}
--- src/main/java/com/github/scompo/notifer/api/runners/NotifierRunner.java
+++
@@ -1,7 +0,0 @@
-package com.github.scompo.notifer.api.runners;
-
-import com.github.scompo.utils.startable.Startable;
-
-public interface NotifierRunner extends Startable {
-
-}
